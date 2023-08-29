7ca5032b25d01d329560219873793b649bb1193b
AH-Likhon <ictianlikhon6@gmail.com>
2023-08-29 09:30:33 +0600

    [DONE] added user name field

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -6,6 +6,10 @@ import config from '../../../config';
 
 const UserSchema = new Schema<IUser>(
   {
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,8 +2,11 @@ import { z } from 'zod';
 
 const createUserZodSchema = z.object({
   body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
     email: z.string({
-      required_error: 'Emailr is required',
+      required_error: 'Email is required',
     }),
     password: z.string().optional(),
   }),
